8ef0679efd2639a3b29696abc9c4c55597d39b7d
Lukas Esterhuizen <esterhuizen2k@proton.me>
2025-12-16 11:36:50 +0200

    Add client/src/components/examples/LoginForm.tsx

---
+++ client/src/components/examples/LoginForm.tsx
@@ -0,0 +1,16 @@
+import { LoginForm } from "../auth/LoginForm";
+import { ThemeProvider } from "../ThemeProvider";
+
+export default function LoginFormExample() {
+  return (
+    <ThemeProvider>
+      <div className="flex min-h-[400px] items-center justify-center bg-background p-8">
+        <LoginForm
+          onLogin={(email, pwd) => console.log("Login:", email, pwd)}
+          onSocialLogin={(provider) => console.log("Social:", provider)}
+          onSignUp={() => console.log("Signup clicked")}
+        />
+      </div>
+    </ThemeProvider>
+  );
+}
