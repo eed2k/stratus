51d63e4f25e8f5aee1a1cd2efd838b7d543dba6d
Lukas Esterhuizen <esterhuizen2k@proton.me>
2025-12-16 11:36:49 +0200

    Add client/src/components/examples/EToCard.tsx

---
+++ client/src/components/examples/EToCard.tsx
@@ -0,0 +1,17 @@
+import { EToCard } from "../dashboard/EToCard";
+import { ThemeProvider } from "../ThemeProvider";
+
+export default function EToCardExample() {
+  return (
+    <ThemeProvider>
+      <div className="p-4 bg-background">
+        <EToCard
+          dailyETo={4.85}
+          weeklyETo={32.4}
+          monthlyETo={128.5}
+          irrigationTime={25}
+        />
+      </div>
+    </ThemeProvider>
+  );
+}
